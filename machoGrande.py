a99c5305e3c8d1a6a4ccbf055cd8b88fceb29142
Chris Smith <smitty1egh@github.com>
2018-01-06 12:45:37 +0000

    Totally refactored machoGrande; need to test.

--- machoGrande.py
+++ machoGrande.py
@@ -9,6 +9,7 @@ from   yaml              import load,Loader
 #WARNING: Do not run machoGrande within a virtual
 #  environment. Pipenv cannot get over machoGrande
 #  running in a venv.
+#TODO: detect venv and bomb out if in one.
 #Just duplicate a session and CD to machoGrande and go
 
 #Assert: virtualenvmanager     installed
@@ -17,20 +18,41 @@ from   yaml              import load,Loader
 #        cookiecutter template configured locally
 #        inputs entered        via CONFIG_SPEC
 
-ROOT_PATH=os.environ['WORKON_HOME']
-TEMPLATE_PATH=ROOT_PATH + 'machoGrande/machoGrande/ccs/'
+ROOT_PATH     =os.environ['WORKON_HOME']
+TEMPLATE_PATH =ROOT_PATH + 'machoGrande/machoGrande/ccs/'
 DEFAULT_CUTTER='cookiecutter-pybase/'
+CONFIG_SPEC   =ROOT_PATH + '/machoGrande/machoGrande/machoGrande.json'
 
-#TODO: get rid of JSON config_spec. Load YAML to obtain PROJ_NAME,
-#  then co[y to TEMPLATE_PATH as cookiecutter.yaml.
-CONFIG_SPEC=ROOT_PATH + '/machoGrande/machoGrande/machoGrande.json'
-config={}
-with open(CONFIG_SPEC,'r') as f:
-    config=load(f.read(),Loader=Loader)
-proj_name=config['project_slug']
+def step_zero_obtain_project_name():
+    '''Peek at machoGrande.json for the name of the project.
+    '''
+    config        ={}
+    with open(CONFIG_SPEC,'r') as f:
+        config=load(f.read(),Loader=Loader)
+    return config['project_slug']
 
-def adjust_location():
-    '''Pipenv's design, while making great sense,
+def step_one_create_project_directory():
+    '''Here we go to our WORKON_HOME location and make the
+       project directory.
+       TODO: test for its existence, and either bomb out or
+         blow away the directory, optionally tarballing it
+         if desired.
+       Here we're going to browbeat pipenv and return the
+         directory it *actually* created.
+    '''
+    projdir=Path(ROOT_PATH,proj_name)
+    os.mkdir(projdir) #placebo
+    subprocess.run(['pipenv','--three'],cwd=projdir)
+
+    #Globbing returns a generator, we know it's a 1-element
+    #  comprehension; grab the first element #FTW
+    realdir=[p for p in Path(ROOT_PATH).glob('%s-*' % proj_name)][0]
+    return realdir
+
+def step_two_adjust_project_location(realdir):
+    '''Move the Pipfile to realdir; rmdir projdir, rename
+       realdir to projdir
+       Pipenv's design, while making great sense,
          is not what we prefer. After moving the
          venv to the desired location, eliminate
          the random characters at the end of the
@@ -43,6 +65,9 @@ def adjust_location():
        ,'/bin/pip3'             ,'/bin/pip3.6'
        ,'/bin/python-config'    ,'/bin/wheel'
        ]
+    shutil.move('%s/Pipfile' % projdir, realdir)
+    os.rmdir(projdir)
+    shutil.move(realdir, projdir)
 
     len_pdir=len(str(projdir))
     naughty_chars=str(realdir)[len_pdir:]
@@ -57,34 +82,33 @@ def adjust_location():
                 else:
                     outfile.write(line)
 
+def step_three_invoke_cookiecutter():
+    '''Overwrite the cookiecutter inputs with the
+         machoGrande.json, then initialize the project.
+       Let's admit that this approach understands cookiecutter
+         about as well as it understands pipenv
+    '''
+    shutil.copy(CONFIG_SPEC
+               ,TEMPLATE_PATH+DEFAULT_CUTTER+'cookiecutter.json')
+    cookiecutter(TEMPLATE_PATH+DEFAULT_CUTTER
+                ,checkout=None,no_input=True
+                ,extra_context=None,replay=False
+                ,overwrite_if_exists=True
+                ,output_dir=ROOT_PATH)
 
-#Make the virtual envrionment, which arrives in a
-#  sort of parallel universe, which we will then inhabit.
-projdir=Path(ROOT_PATH,proj_name)
-os.mkdir(projdir) #placebo
-subprocess.run(['pipenv','--three'],cwd=projdir)
+def step_four_gittify_project():
+    '''Invoke git, and do the first commit.
+    '''
+    subprocess.run(['git','init'      ],cwd=projdir)
+    subprocess.run(['git','add'   ,'.'],cwd=projdir)
+    subprocess.run(['git','commit','-a'
+                   ,'-m' ,'Over Macho Grande?'],cwd=projdir)
 
-#Globbing returns a generator, we know it's a 1-element
-#  comprehension; grab the first element #FTW
-realdir=[p for p in Path(ROOT_PATH).glob('%s-*' % proj_name)][0]
-print(' the realdir is %s' % realdir)
-
-#Move the Pipfile to realdir; rmdir projdir, rename realdir to projdir
-shutil.move('%s/Pipfile' % projdir, realdir)
-os.rmdir(projdir)
-shutil.move(realdir, projdir)
-adjust_location()
-
-
-#Overwrite the cookiecutter inputs with the machoGrande.json,
-#  then initialize the project.
-shutil.copy(CONFIG_SPEC,TEMPLATE_PATH+DEFAULT_CUTTER+'cookiecutter.json')
-cookiecutter(TEMPLATE_PATH+DEFAULT_CUTTER,checkout=None,no_input=True
-            ,extra_context=None,replay=False,overwrite_if_exists=True
-            ,output_dir=ROOT_PATH)
-
-##Invoke git, and do the first commit.
-subprocess.run(['git','init'      ],cwd=projdir)
-subprocess.run(['git','add'   ,'.'],cwd=projdir)
-subprocess.run(['git','commit','-a'
-               ,'-m' ,'Over Macho Grande?'],cwd=projdir)
+if __name__=='__main__':
+    proj_name=step_zero_obtain_project_name()
+    real_dir =step_one_create_project_directory(proj_name)
+    step_two_adjust_project_location(real_dir)
+    step_three_invoke_cookiecutter()
+    step_four_gittify_project()
+    #TODO
+    #setp_six_push_to_github
